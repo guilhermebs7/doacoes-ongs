b3630bd511e8041813630f886ba1848685b1647a
guilhermebs7 <gbs5@cin.ufpe.br>
2025-07-03 20:51:13 -0300

    Update Item.cpp

--- backend/src/Item.cpp
+++ backend/src/Item.cpp
@@ -1,12 +1,12 @@
-#include "Item.h"
+#include "Item.h"  // inclui a definição da classe Item e suas dependências
 #include <iostream>
 using namespace std;
 
-Item::Item(long id, const string& nome, const string& descricao,
+Item::Item(long id, const string& nome, const string& descricao,         // construtor da classe Item,inicializa o item com os parâmetros fornecidos
            Categoria categoria, Doador* dono, const string& cidade)
     : id(id), categoria(categoria), status(Status::DISPONIVEL), dono(dono) {
 
-    if (nome.empty()) {
+    if (nome.empty()) {                // validação: nome não pode ser vazio
         throw invalid_argument("O nome do item não pode estar em branco.");
     }
     if (descricao.empty()) {
@@ -21,17 +21,17 @@ Item::Item(long id, const string& nome, const string& descricao,
     this->cidade = cidade;
 }
 
-long Item::getId() const { return id; }
+long Item::getId() const { return id; }   //retorna id do item
 
-string Item::getNome() const { return nome; }
+string Item::getNome() const { return nome; }  // nome do item
 
-Doador* Item::getDono() const { return dono; }
+Doador* Item::getDono() const { return dono; }  // retorna um ponteiro para o doador do item
 
-void Item::alterarStatus(Status novoStatus) {
+void Item::alterarStatus(Status novoStatus) {    // altera o status atual do item para o novo status fornecido
     status = novoStatus;
 }
 
-void Item::cancelarReserva() {
+void Item::cancelarReserva() {              // Cancela uma reserva caso o item esteja reservado 
     if (status == Status::RESERVADO) {
         status = Status::DISPONIVEL;
         cout << "Reserva cancelada com sucesso para o item: " << nome << endl;
@@ -40,7 +40,7 @@ void Item::cancelarReserva() {
     }
 }
 
-void Item::exibirItem() const {
+void Item::exibirItem() const {               //exibe as informações do item no console
     cout << "ID: " << id
          << "\nNome: " << nome
          << "\nDescrição: " << descricao
@@ -49,7 +49,7 @@ void Item::exibirItem() const {
          << "\nCidade: " << cidade << "\n";
 }
 
-string Item::statusParaString(Status s) const {
+string Item::statusParaString(Status s) const {      // converte o status enum em uma string para exibição
     switch (s) {
         case Status::DISPONIVEL: return "Disponível";
         case Status::RESERVADO: return "Reservado";
@@ -58,7 +58,7 @@ string Item::statusParaString(Status s) const {
     }
 }
 
-string Item::categoriaParaString(Categoria c) const {
+string Item::categoriaParaString(Categoria c) const {    // converte a categoria enum em uma string para exibição
     switch (c) {
         case Categoria::ALIMENTO: return "Alimento";
         case Categoria::ROUPA: return "Roupa";
