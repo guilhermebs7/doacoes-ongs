d0d15dabb8d95830d8d77a554b2a1cb6581d99aa
rodrigoadn0 <rodrigobala727@gmail.com>
2025-07-04 16:32:58 -0300

    Update Usuario.cpp

--- backend/src/Usuario.cpp
+++ backend/src/Usuario.cpp
@@ -8,23 +8,23 @@ Usuario::Usuario(const std::string& nome, const std::string& email, const std::s
 std::string Usuario::getNome() const {
     return nome;
 }
-
+// Atualiza o nome do usuário
 void Usuario::setNome(const std::string& nome) {
     this->nome = nome;
 }
-
+// Retorna o email do usuário
 std::string Usuario::getEmail() const {
     return email;
 }
-
+// Atualiza o email do usuário
 void Usuario::setEmail(const std::string& email) {
     this->email = email;
 }
-
+// Retorna a senha do usuário
 std::string Usuario::getSenha() const {
     return senha;
 }
-
+// Atualiza a senha do usuário
 void Usuario::setSenha(const std::string& senha) {
     this->senha = senha;
 }
